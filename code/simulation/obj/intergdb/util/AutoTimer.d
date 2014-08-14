d1494b3de0bc75da80525feebe23b46ab53ae6be
Robert Soule <soule@cs.cornell.edu>
2014-08-14 13:15:45 +0200

    removed .d files

--- code/simulation/obj/intergdb/util/AutoTimer.d
+++
@@ -1,4 +0,0 @@
-obj/intergdb/util/AutoTimer.o: src/intergdb/util/AutoTimer.cpp \
-  include/intergdb/util/AutoTimer.h
-
-include/intergdb/util/AutoTimer.h:
--- code/simulation/obj/intergdb/util/ZipfRand.d
+++
@@ -1,4 +0,0 @@
-obj/intergdb/util/ZipfRand.o: src/intergdb/util/ZipfRand.cpp \
-  include/intergdb/util/ZipfRand.h
-
-include/intergdb/util/ZipfRand.h:
