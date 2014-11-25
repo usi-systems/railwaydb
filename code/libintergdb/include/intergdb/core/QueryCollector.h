9f032ece93a5066f01c074a2c56846107ec1da54
Robert Soule <soule@cs.cornell.edu>
2014-11-25 18:52:40 +0100

    checkpoint. working on naive bucketing.

--- code/libintergdb/include/intergdb/core/QueryCollector.h
+++ code/libintergdb/include/intergdb/core/QueryCollector.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <intergdb/core/Query.h>
-
+#include <math.h>
 #include <iostream>
 
 namespace intergdb { namespace core
@@ -11,8 +11,9 @@ namespace intergdb { namespace core
 
     class Bucketer
     {
+    protected:
     public:
-        Bucketer() { }        
+        Bucketer() { }
         virtual BucketId getBucket(Query q) { 
             std::cout << "Bucketer::getBucket" << std::endl;            
                 return -1; 
@@ -22,22 +23,30 @@ namespace intergdb { namespace core
     class NaiveBucketer : public Bucketer
     {
     public:
-        NaiveBucketer() {}
+        NaiveBucketer(int bucketSize) { }        
         virtual BucketId getBucket(Query q) { 
             std::cout << "NaiveBucketer::getBucket" << std::endl;
+            Timestamp start = q.getStart();
+            Timestamp end = q.getEnd();
+            std::cout << "NaiveBucketer::getBucket " << start << " " << end << std::endl;
+            int firstBucket = 0.0 == fmod(start, bucketSize_) ? (start / bucketSize_) :  (start / bucketSize_ + 1);
+            int lastBucket = 0.0 == fmod(end, bucketSize_) ? (end / bucketSize_) :  (end / bucketSize_ + 1);
+            std::cout << "NaiveBucketer::getBucket firstBucket " << firstBucket << std::endl;
+            std::cout << "NaiveBucketer::getBucket lastBucket " << lastBucket << std::endl;
             return 1; 
         } 
+    private:
+        int bucketSize_;
     };
 
     class QueryCollector
     {
     public:
-    QueryCollector() : bucketer_(NaiveBucketer()) {}
+    QueryCollector() : bucketer_(1) { }
         void collectIntervalQuery(Query q);
         void collectFocusedIntervalQuery(Query q);
     private:
-        // TODO: (rjs): change this to a pointer to a bucketer?
-        Bucketer bucketer_;
+        NaiveBucketer bucketer_;
     };
 
 } } /* namespace */
