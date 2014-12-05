23d49e489a7f59b299f925d30cbfa4d273ed3218
Robert Soule <soule@cs.cornell.edu>
2014-12-05 11:55:13 +1100

    Mapping a query to a set of buckets.

--- code/libintergdb/include/intergdb/core/QueryCollector.h
+++ code/libintergdb/include/intergdb/core/QueryCollector.h
@@ -14,34 +14,26 @@ namespace intergdb { namespace core
     protected:
     public:
         Bucketer() { }
-        virtual BucketId getBucket(Query q) { 
-            std::cout << "Bucketer::getBucket" << std::endl;            
-                return -1; 
-        }
+        virtual std::vector<BucketId> getBuckets(Query q) = 0; 
     };
 
     class NaiveBucketer : public Bucketer
     {
     public:
-        NaiveBucketer(int bucketSize) { }        
-        virtual BucketId getBucket(Query q) { 
-            #ifdef DEBUG
-            std::cout << "NaiveBucketer::getBucket" << std::endl;
+        NaiveBucketer(int bucketSize) : bucketSize_(bucketSize) { }        
+        virtual std::vector<BucketId> getBuckets(Query q) { 
+            std::vector<BucketId> bucketIds;
             Timestamp start = q.getStart();
             Timestamp end = q.getEnd();
-            std::cout << "NaiveBucketer::getBucket " << start << " " << end << std::endl;
             int firstBucket = 0.0 == fmod(start, bucketSize_) ? (start / bucketSize_) :  (start / bucketSize_ + 1);
             int lastBucket = 0.0 == fmod(end, bucketSize_) ? (end / bucketSize_) :  (end / bucketSize_ + 1);
-            std::cout << "NaiveBucketer::getBucket firstBucket " << firstBucket << std::endl;
-            std::cout << "NaiveBucketer::getBucket lastBucket " << lastBucket << std::endl;
-            #endif
-            return 1; 
+            for (int i = firstBucket; i <= lastBucket; i += bucketSize_) {
+                bucketIds.push_back(i);
+            }
+            return bucketIds; 
         } 
     private:
-#ifdef DEBUG
         int bucketSize_;
-#endif
-
     };
 
     class QueryCollector
--- code/libintergdb/src/intergdb/core/QueryCollector.cpp
+++ code/libintergdb/src/intergdb/core/QueryCollector.cpp
@@ -6,10 +6,13 @@ using namespace intergdb::core;
 
 void QueryCollector::collectIntervalQuery(Query q) 
 {
-    // We need something that takes a query, and maps it to a bucket
+    // We need something that takes a query, and maps it to a set of buckets
     // The bucket will be determined in different ways....
-    std::cout << "QueryCollector::collectIntervalQuery" << std::endl;
-    bucketer_.getBucket(q);
+    //std::cout << "QueryCollector::collectIntervalQuery" << std::endl;
+    std::vector<BucketId> bucketIds = bucketer_.getBuckets(q);
+    //for (auto i : bucketIds) {
+    //    std::cout << "QueryCollector::collectIntervalQuery" << " " << i << std::endl;
+    //}
     
 }
 
