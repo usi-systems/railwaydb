3e70b199a9ba0131aca20cec2a9ca2ea37c67513
Robert Soule <soule@cs.cornell.edu>
2015-01-05 21:08:21 +0100

    ClusteringCoefficientTest done.

---
+++ code/test/src/ClusteringCoefficientTest/ClusteringCoefficientTest.cpp
@@ -0,0 +1,132 @@
+#include "gtest/gtest.h"  
+
+#include <intergdb/core/InteractionGraph.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <boost/filesystem.hpp>
+
+using namespace std;
+using namespace intergdb::core;
+
+
+class ClusteringCoefficientTest : public ::testing::Test 
+{
+public:
+    ClusteringCoefficientTest() {}
+protected:
+    virtual void SetUp() 
+        {
+            auto storageDir = boost::filesystem::unique_path("/tmp/mydb_%%%%");
+            conf.reset(new Conf("test", "/tmp/myigdb_cc", 
+                                {{"vertex-label",DataType::STRING}}, 
+                                {{"a", DataType::STRING}}));
+            if (boost::filesystem::exists(conf->getStorageDir()))
+                boost::filesystem::remove_all(conf->getStorageDir());
+            boost::filesystem::create_directories(conf->getStorageDir());   
+            graph.reset(new InteractionGraph(*conf));
+        }
+    virtual void TearDown() 
+        { 
+            boost::filesystem::remove_all(graph->getConf().getStorageDir());
+        }
+    unordered_map<VertexId, double> findClusteringCoefficients(
+        InteractionGraph & graph, double startTime, double endTime, std::vector<std::string> attributes)
+        {
+            vector<VertexId> vertices;
+            IntervalQuery iq(startTime, endTime);
+            for (auto iqIt = graph.processIntervalQuery(iq); 
+                 iqIt.isValid(); iqIt.next()) 
+                vertices.push_back(iqIt.getVertexId()); 
+
+            unordered_map<VertexId, double> clusteringCoefficients;
+            for (VertexId v : vertices) {
+                double clusteringCoefficient = 0.0;
+                unordered_set<VertexId> tz;
+                FocusedIntervalQuery fiq1(v, startTime, endTime, attributes);
+                for (auto fiqIt = graph.processFocusedIntervalQuery(fiq1); 
+                     fiqIt.isValid(); fiqIt.next()) {
+                    auto to = fiqIt.getToVertex();
+                    tz.insert(to);
+                }
+                for (VertexId const & ov : tz) {
+                    FocusedIntervalQuery fiq2(ov, startTime, endTime, attributes);
+                    for (auto fiqIt = graph.processFocusedIntervalQuery(fiq2); 
+                         fiqIt.isValid(); fiqIt.next()) {
+                        auto to = fiqIt.getToVertex();
+                        if (tz.count(to)>0)
+                            clusteringCoefficient++;
+                    }
+                }
+                size_t z = tz.size();
+                if (z<2)
+                    clusteringCoefficient = 0.0;
+                else
+                    clusteringCoefficient /= (z*(z-1)/2.0);
+                clusteringCoefficients[v] = clusteringCoefficient;
+            }
+            return clusteringCoefficients;
+        }
+
+
+protected:
+    std::unique_ptr<Conf> conf;
+    std::unique_ptr<InteractionGraph> graph;
+};
+
+TEST_F(ClusteringCoefficientTest, WriteReadTest) 
+{
+
+    graph->createVertex(1, "v1"); 
+    graph->createVertex(2, "v2"); 
+    graph->createVertex(3, "v3");
+    graph->createVertex(4, "v4");
+    Timestamp ts = 1.0;
+    graph->addEdge(1, 3, ts++, "e1-3");
+    graph->addEdge(1, 4, ts++, "e1-4");
+    graph->addEdge(2, 3, ts++, "e2-3");
+    graph->addEdge(2, 4, ts++, "e2-4");
+    graph->addEdge(3, 4, ts++, "e3-4");
+    graph->flush();
+        
+    double startTime = 0.0, endTime = 2.0;
+    std::vector<std::string> attributes = {"a"};
+
+    auto clusteringCoefficients = findClusteringCoefficients(*graph, startTime, endTime, attributes);
+
+    auto it = clusteringCoefficients.find (1);
+    EXPECT_NE (it, clusteringCoefficients.end());
+    EXPECT_EQ(it->second, 0);
+
+    it = clusteringCoefficients.find (3);
+    EXPECT_NE(it, clusteringCoefficients.end());
+    EXPECT_EQ(it->second, 0);
+
+    endTime += 5.0;
+    clusteringCoefficients = findClusteringCoefficients(*graph, startTime, endTime, attributes);
+
+    it = clusteringCoefficients.find(1);
+    EXPECT_NE (it, clusteringCoefficients.end());
+    EXPECT_EQ(it->second, 2);
+
+    it = clusteringCoefficients.find(2);
+    EXPECT_NE(it, clusteringCoefficients.end());
+    EXPECT_EQ(it->second, 2);
+
+    it = clusteringCoefficients.find(3);
+    EXPECT_NE(it, clusteringCoefficients.end());
+    EXPECT_EQ(true, (fabs(it->second - 1.3333) < 0.1));
+
+    it = clusteringCoefficients.find(4);
+    EXPECT_NE(it, clusteringCoefficients.end());
+    EXPECT_EQ(true, (fabs(it->second - 1.3333) < 0.1));
+
+
+}
+
+
+
--- code/test/src/SpanningTreeTest/SpanningTreeTest.cpp
+++ code/test/src/SpanningTreeTest/SpanningTreeTest.cpp
@@ -1,3 +1,5 @@
+#include "gtest/gtest.h"  
+
 #include <intergdb/core/InteractionGraph.h>
 
 #include <cstdlib>
@@ -15,105 +17,135 @@ using boost::lexical_cast;
 
 #define MAX_WT std::numeric_limits<double>::max()
 
-void createGraph(InteractionGraph &graph) 
+class SpanningTreeTest : public ::testing::Test 
 {
-    graph.createVertex(0, "v0");
-    graph.createVertex(1, "v1");
-    graph.createVertex(2, "v2");
-    graph.createVertex(3, "v3");
-    graph.createVertex(4, "v4");
-    graph.createVertex(5, "v5");
-    graph.createVertex(6, "v6");
-    graph.createVertex(7, "v7");
-    Timestamp ts = 1.0;
-    graph.addEdge(3, 5, ts, "1"); 
-    graph.addEdge(0, 2, ts, "2");
-    graph.addEdge(0, 7, ts, "3");
-    graph.addEdge(0, 1, ts, "4");
-    graph.addEdge(3, 4, ts, "5");
-    graph.addEdge(5, 4, ts, "6"); 
-    graph.addEdge(0, 6, ts, "7");
-    graph.addEdge(7, 1, ts, "8");
-    graph.addEdge(7, 6, ts, "9");
-    graph.addEdge(7, 4, ts, "10");
-    graph.addEdge(6, 4, ts, "11");
-    graph.addEdge(0, 5, ts, "12");
-    graph.flush();     
-}
+public:
+    SpanningTreeTest() {}   
+protected:
+    virtual void SetUp() 
+        {
+            auto storageDir = boost::filesystem::unique_path("/tmp/mydb_%%%%");
+            conf.reset(new Conf("test", "/tmp/myigdb_st", 
+                       {{"vertex-label", DataType::STRING}}, 
+                                {{"a", DataType::STRING}}));
 
-int msp(InteractionGraph &graph, VertexId q0, set<pair<VertexId,VertexId> > &tree,
-        double startTime, double endTime, std::vector<std::string> const & attributes)
-{
-    std::set<VertexId> out; // The set of vertices not in the tree
-    std::set<VertexId> in;  // The set of vertices in the tree
-    VertexId v;
-    VertexId u;
-
-    int i = 0;
-    IntervalQuery q1(startTime, endTime);
-    for (auto iqIt = graph.processIntervalQuery(q1); iqIt.isValid(); iqIt.next()) {
-        if (i == 0) {
-            v = iqIt.getVertexId();
+            if (boost::filesystem::exists(conf->getStorageDir()))
+                boost::filesystem::remove_all(conf->getStorageDir());
+            boost::filesystem::create_directories(conf->getStorageDir());   
+            graph.reset(new InteractionGraph(*conf));
         }
-        out.insert(iqIt.getVertexId());
-        i++;
-    }
+    virtual void TearDown() 
+        { 
+            boost::filesystem::remove_all(graph->getConf().getStorageDir());
+        }
+
+    void msp(InteractionGraph &graph, VertexId q0, set<pair<VertexId,VertexId> > &tree,
+             double startTime, double endTime, std::vector<std::string> const & attributes)
+        {
+            std::set<VertexId> out; // The set of vertices not in the tree
+            std::set<VertexId> in;  // The set of vertices in the tree
+            VertexId v;
+            VertexId u;
+
+            int i = 0;
+            IntervalQuery q1(startTime, endTime);
+            for (auto iqIt = graph.processIntervalQuery(q1); iqIt.isValid(); iqIt.next()) {
+                if (i == 0) {
+                    v = iqIt.getVertexId();
+                }
+                out.insert(iqIt.getVertexId());
+                i++;
+            }
 
  
-    double minWeight = MAX_WT;
-    double edgeWeight = MAX_WT;
-    VertexId minDst = v;
-    VertexId minSrc = v;
+            double minWeight = MAX_WT;
+            double edgeWeight = MAX_WT;
+            VertexId minDst = v;
+            VertexId minSrc = v;
 
-    while (!out.empty()) {                
-        in.insert(minDst);       // add v to the tree.        
-        out.erase(minDst);       // remove v from "not in the tree"
-        minWeight = MAX_WT;
-        for (auto it=in.begin(); it!=in.end(); ++it) {
-            u = *it;
-            FocusedIntervalQuery fiq(u, startTime, endTime, attributes);
-            for (auto fiqIt = graph.processFocusedIntervalQuery(fiq); fiqIt.isValid(); fiqIt.next()) {
-                v = fiqIt.getToVertex();               
-                if (in.find(v) == in.end()) {
-                    edgeWeight = lexical_cast<double>(*fiqIt.getEdgeData());
-                    if (edgeWeight <= minWeight) {
-                        minWeight = edgeWeight;
-                        minDst = v;
-                        minSrc = u;
-                    }                                       
+            while (!out.empty()) {                
+                in.insert(minDst);       // add v to the tree.        
+                out.erase(minDst);       // remove v from "not in the tree"
+                minWeight = MAX_WT;
+                for (auto it=in.begin(); it!=in.end(); ++it) {
+                    u = *it;
+                    FocusedIntervalQuery fiq(u, startTime, endTime, attributes);
+                    for (auto fiqIt = graph.processFocusedIntervalQuery(fiq); fiqIt.isValid(); fiqIt.next()) {
+                        v = fiqIt.getToVertex();               
+                        if (in.find(v) == in.end()) {
+                            edgeWeight = lexical_cast<double>(*fiqIt.getEdgeData());
+                            if (edgeWeight <= minWeight) {
+                                minWeight = edgeWeight;
+                                minDst = v;
+                                minSrc = u;
+                            }                                       
+                        }
+                    }
                 }
+                tree.insert(std::make_pair (minSrc,minDst));        
             }
         }
-        tree.insert(std::make_pair (minSrc,minDst));        
-    }
 
-    return 0;
-}
 
-int main()
+
+protected:
+    std::unique_ptr<Conf> conf;
+    std::unique_ptr<InteractionGraph> graph;
+};
+
+
+TEST_F(SpanningTreeTest, WriteReadTest) 
 {
-    Conf conf("test", "/tmp/myigdb_st", 
-        {{"vertex-label", DataType::STRING}}, 
-        {{"a", DataType::STRING}});
-    bool newDB = !boost::filesystem::exists(conf.getStorageDir());
-    boost::filesystem::create_directories(conf.getStorageDir());
+    graph->createVertex(0, "v0");
+    graph->createVertex(1, "v1");
+    graph->createVertex(2, "v2");
+    graph->createVertex(3, "v3");
+    graph->createVertex(4, "v4");
+    graph->createVertex(5, "v5");
+    graph->createVertex(6, "v6");
+    graph->createVertex(7, "v7");
+    Timestamp ts = 1.0;
+    graph->addEdge(3, 5, ts, "1"); 
+    graph->addEdge(0, 2, ts, "2");
+    graph->addEdge(0, 7, ts, "3");
+    graph->addEdge(0, 1, ts, "4");
+    graph->addEdge(3, 4, ts, "5");
+    graph->addEdge(5, 4, ts, "6"); 
+    graph->addEdge(0, 6, ts, "7");
+    graph->addEdge(7, 1, ts, "8");
+    graph->addEdge(7, 6, ts, "9");
+    graph->addEdge(7, 4, ts, "10");
+    graph->addEdge(6, 4, ts, "11");
+    graph->addEdge(0, 5, ts, "12");
+    graph->flush();     
 
-    set<pair<VertexId,VertexId> > tree;
-    InteractionGraph graph(conf);
-
-    if (newDB) 
-        createGraph(graph);
 
     double startTime = 0.0, endTime = 2.0;
     std::vector<std::string> attributes = {"a"};
-    msp(graph, 0, tree, startTime, endTime, attributes);
+    set<pair<VertexId,VertexId> > tree;
 
-    for (auto it=tree.begin(); it!=tree.end(); ++it) {
-        cout << it->first << "->" <<it->second << " in tree" << endl;
-    }
+    msp(*graph, 0, tree, startTime, endTime, attributes);
 
-    return EXIT_SUCCESS;
+    auto it = tree.find(make_pair(0, 1));
+    EXPECT_NE(it, tree.end());
+
+    it = tree.find(make_pair(0, 6));
+    EXPECT_NE(it, tree.end());
+
+    it = tree.find(make_pair(0, 7));
+    EXPECT_NE(it, tree.end());
+
+    it = tree.find(make_pair(2, 0));
+    EXPECT_NE(it, tree.end());
+
+    it = tree.find(make_pair(3, 5));
+    EXPECT_NE(it, tree.end());
+
+    it = tree.find(make_pair(4, 3));
+    EXPECT_NE(it, tree.end());
+
+    it = tree.find(make_pair(7, 4));
+    EXPECT_NE(it, tree.end());
+
+
 }
-
-
-
