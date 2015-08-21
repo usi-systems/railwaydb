8b572aadaa9f90221fb3139e63abcf5c47929a50
Robert Soulé <robert.soule@usi.ch>
2015-08-21 10:07:10 +0200

    Visualization scripts for time experiments

---
+++ code/simulation/viz/RunningTimeVsBlockSize.py
@@ -0,0 +1,41 @@
+import CommonConf
+import CommonViz
+
+import re
+from collections import OrderedDict
+import numpy as np
+import matplotlib.pyplot as pp
+import matplotlib
+
+EXPERIMENT_NAME = "RunningTimeVsBlockSize"
+X_LABEL         = "Block Size (bytes)"
+Y_LABEL         = "Time (sec.)"
+            
+def main(dirn, fname): 
+  (xs, ysPerSolver, ydevsPerSolver) = CommonViz.parseData(dirn, fname)
+     
+  CommonConf.setupMPPDefaults()
+  fmts = CommonConf.getLineFormats()
+  mrkrs = CommonConf.getLineMarkers()
+  fig = pp.figure()
+  ax = fig.add_subplot(111)
+  ax.set_xscale("log", basex=2)
+
+  index = 0
+  for (solver, ys), (solver, ydevs) in zip(ysPerSolver.iteritems(),ydevsPerSolver.iteritems()) : 
+    ax.errorbar(xs, ys, yerr=ydevs, label=solver, marker=mrkrs[index], linestyle=fmts[index])
+    index = index + 1
+
+  ax.set_xlabel(X_LABEL);
+  ax.set_ylabel(Y_LABEL);
+  ax.legend(loc='best', fancybox=True)
+
+  ax.get_yaxis().set_major_formatter(
+    matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x/1000000), ',')))
+
+  pp.savefig(dirn+"/"+fname+".pdf")
+  pp.show()
+
+if __name__ == "__main__":
+  main("expData", EXPERIMENT_NAME)
+
---
+++ code/simulation/viz/RunningTimeVsTimeDeltaDFS.py
@@ -0,0 +1,44 @@
+import CommonConf
+import CommonViz
+
+import re
+from collections import OrderedDict
+import numpy as np
+import matplotlib.pyplot as pp
+import matplotlib
+
+EXPERIMENT_NAME = "RunningTimeVsTimeDeltaDFS"
+X_LABEL         = "Time Delta (% of total graph time span)"
+Y_LABEL         = "Time (sec.)"
+            
+def main(dirn, fname): 
+  (xs, ysPerSolver, ydevsPerSolver) = CommonViz.parseData(dirn, fname)
+     
+  CommonConf.setupMPPDefaults()
+  fmts = CommonConf.getLineFormats()
+  mrkrs = CommonConf.getLineMarkers()
+  fig = pp.figure()
+  ax = fig.add_subplot(111)
+  # ax.set_xscale("log", basex=2)
+
+  index = 0
+  for (solver, ys), (solver, ydevs) in zip(ysPerSolver.iteritems(),ydevsPerSolver.iteritems()) : 
+    ax.errorbar(xs, ys, yerr=ydevs, label=solver, marker=mrkrs[index], linestyle=fmts[index])
+    index = index + 1
+
+  ax.set_xlabel(X_LABEL);
+  ax.set_ylabel(Y_LABEL);
+  ax.legend(loc='best', fancybox=True)
+
+  ax.get_yaxis().set_major_formatter(
+    matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x/1000000), ',')))
+
+  ax.get_xaxis().set_major_formatter(
+    matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x * 100), ',')))
+  
+  pp.savefig(dirn+"/"+fname+".pdf")
+  pp.show()
+
+if __name__ == "__main__":
+  main("expData", EXPERIMENT_NAME)
+
